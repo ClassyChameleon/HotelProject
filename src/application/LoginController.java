4f576d4b63ab7cbf48c1d32eed087b49dd2f0803
gvg8 <gvg8@hi.is>
2021-05-11 14:11:53 +0000

    SQLite test successful!
    Java successfully executed a select query on a database using SQLite.
    The results were displayed correctly twice so I must conclude that it's
    working as intended.
    My next mission is to create a user interface (that'll take quite a
    while)

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,5 +1,44 @@
 package application;
 
+import java.sql.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LoginController {
+	@FXML
+	private Button button;
 	
+	public void buttonPressed() throws Exception {
+		Class.forName("org.sqlite.JDBC");
+		Connection conn = null;
+		try
+		{
+			conn = DriverManager.getConnection("jdbc:sqlite:hotel.db");
+			Statement statement = conn.createStatement();
+			
+			ResultSet rs = statement.executeQuery("select * from Hotel");
+			while(rs.next())
+			{
+				System.out.println("hotelName = " + rs.getString("Name"));
+				System.out.println("hotelLocation = " + rs.getString("Location"));
+			}
+			rs.close();
+		}
+		catch(SQLException e)
+		{
+			System.err.println(e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				if (conn != null)
+					conn.close();
+			}
+			catch(SQLException e)
+			{
+				System.err.println(e);
+			}
+		}
+	}
 }
